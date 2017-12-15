5edc4e02fdfc8103b57bbb61f57ed668a55ba574
Ilya Ruby <itimofeev@imap.cc>
2017-12-15 10:44:02 +0300

    let's get use of shared_context benefits

--- spec/unit/sample_importer_spec.rb
+++ spec/unit/sample_importer_spec.rb
@@ -2,6 +2,8 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '../rails_helper'))
 
 describe 'SampleImporter' do
+  include_context 'shared_sample_data'
+
   def categories_taxonomy
     Spree::Taxonomy.find_by(name: "Categories")
   end
@@ -10,15 +12,6 @@ describe 'SampleImporter' do
     Spree::Taxon.find_by(name: "Bags")
   end
 
-  let(:sample_file_contents) do
-    <<~SAMPLE_FILE
-      ;name;description;price;availability_date;slug;stock_total;category
-      ;Ruby on Rails Bag;Animi officia aut amet molestiae atque excepturi. Placeat est cum occaecati molestiae quia. Ut soluta ipsum doloremque perferendis eligendi voluptas voluptatum.;22,99;2017-12-04T14:55:22.913Z;ruby-on-rails-bag;15;Bags
-      ;Spree Bag;Rerum quaerat autem non nihil quo laborum aut hic. Iure adipisci neque eum qui dolor. Velit sed molestias nostrum et dolore. Amet sed repellendus quod et ad.;25,99;2017-12-04T14:55:22.913Z;spree-bag;5;Bags
-      ;Spree Tote;Consequuntur quibusdam repellendus quia non. Iste et pariatur nulla fugit. In ipsum accusantium quasi mollitia et eos. Ullam veniam quis ut adipisci est autem molestiae eos. Ab necessitatibus et rerum quasi quia debitis eum.;14,99;2017-12-30T14:55:22.913Z;spree-tote;20;Bags
-    SAMPLE_FILE
-  end
-  let(:sample_data) { SampleData.new(StringIO.new(sample_file_contents)) }
   let(:imported_product) { Spree::Product.first }
   
   it "imports sample items" do
